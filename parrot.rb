ff413fab410cbed6bedf89056911257fa1cddc85
Simon Lee <simonlee0306@gmail.com>
2018-10-02 23:13:59 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,10 +1,5 @@
 # Create method `parrot` that outputs a given phrase and
-def parrot(str)
-  str1 = str
-  if str1 == "" || str1 == nil
-    str1 = "Squawk!"
-  end
-  puts str1
-  return str1
+def parrot(str = "Squawk!")
+  puts str
 end
 # returns the phrase
